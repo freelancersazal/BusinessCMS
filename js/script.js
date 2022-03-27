34f5e56d0b7df2eb4948da511bbe527164bd86f1
unknown <freelancersazal@gmail.com>
2022-03-27 17:57:24 +0600

    home page updatede

--- js/script.js
+++ js/script.js
@@ -24,11 +24,44 @@ if ($('.main_menu').offset() != undefined) {
 //*==========SEARCH==========  
 $(".wsus__menu_search").click(function(){
     $(".wsus__search_form").toggleClass("show_form");
-  });
+});
 
 
-    
-    
+//*=======BANNER SLIDER====== 
+$('.portfolio_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: false,
+    arrows: true,
+    nextArrow: '<i class="fas fa-chevron-right nxt_arr"></i>',
+    prevArrow: '<i class="fas fa-chevron-left prv_arr"></i>',
+
+
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }   
+    ]
+
+}); 
+
     
     
     
